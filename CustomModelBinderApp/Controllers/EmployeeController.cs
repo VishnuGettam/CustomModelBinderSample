8711aa0f098587d6c46df13befc3e797e0f6f094
VishnuGettam <vishnugettam@gmail.com>
2021-08-13 13:53:54 +0530

    Add project files.

---
+++ CustomModelBinderApp/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using CustomModelBinderApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomModelBinderApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+         
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CreateEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateEmployee( [ModelBinder(typeof(CustomModelBinder))]Employee employee)
+        {
+
+            //Response.Write($" Employee Address - { employee.Address} ");
+
+            return Content($" Employee Address - { employee.Address} ");
+        }
+    }
+}
---
+++ CustomModelBinderApp/Models/CustomModelBinder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomModelBinderApp.Models
+{
+    public class CustomModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            int EmpId = Convert.ToInt32(controllerContext.HttpContext.Request.Form["Id"]);
+            string Name = controllerContext.HttpContext.Request.Form["Name"];
+            string DoorNum = controllerContext.HttpContext.Request.Form["DoorNum"];
+            string Street = controllerContext.HttpContext.Request.Form["Street"];
+            string Place = controllerContext.HttpContext.Request.Form["Place"];
+
+             
+
+            Employee employee = new Employee()
+            {
+                Id = EmpId,
+                Name = Name,
+                Address = DoorNum + "," + Street + "," + Place
+
+            };
+
+            return employee;
+        }
+    }
+}
